fbad646c6ef833e3e1992acfad2055e0ac6ec619
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2021-06-20 12:58:16 -0500

    Initial commit

---
+++ week-4/reactive-composer-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+/*===========================
+; Title: App Component
+; Author: George Henderson
+; Date: 20 June 2021
+; Description: Contains data for App Component.
+;===========================*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment = 'Exercise 4.3 - Handling Events with Observables';
+}
---
+++ week-4/reactive-composer-app/src/app/composer-list/composer-list.component.ts
@@ -0,0 +1,38 @@
+/*===========================
+; Title: Composer List Component
+; Author: George Henderson
+; Date: 20 June 2021
+; Description: Contains class data related to Composer List Component.
+;===========================*/
+
+import { Component, OnInit } from '@angular/core';
+import { IComposer } from '../composer.interface';
+import { ComposerService } from '../composer.service';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-composer-list',
+  templateUrl: './composer-list.component.html',
+  styleUrls: ['./composer-list.component.css']
+})
+export class ComposerListComponent implements OnInit {
+
+  composers: Array<IComposer>;
+  txtSearchControl = new FormControl("");
+
+  constructor( private composerService : ComposerService) {
+    this.composers = this.composerService.getComposers();
+
+    this.txtSearchControl.valueChanges
+    .pipe(debounceTime(500))
+    .subscribe(val => this.filterComposers(val))
+  }
+
+  ngOnInit(): void {
+  }
+
+  filterComposers(name : string) {
+    alert(name);
+  }
+}
