eed989e490725ace302df1a66d8b80e9dd1604af
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2021-07-04 15:07:34 -0500

    initial commit

---
+++ week-6/gpa-calculator-app2/src/app/gpa/gpa.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-gpa',
+  templateUrl: './gpa.component.html',
+  styleUrls: ['./gpa.component.scss']
+})
+export class GpaComponent implements OnInit {
+
+  @Input() gpaTotal : number = 0;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
